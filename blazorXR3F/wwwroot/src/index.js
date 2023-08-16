9efed2f685920bf949965f29082a523b2cf176ed
Amudha <amudha@Amudhas-MacBook-Pro.local>
2023-08-16 11:24:33 -0400

    webassembly version

---
+++ blazorXR3F/wwwroot/src/index.js
@@ -0,0 +1,26 @@
+﻿import React, { useRef } from 'https://esm.sh/react'
+import ReactDOM from 'https://esm.sh/react-dom'
+import { Canvas, useFrame } from 'https://esm.sh/@react-three/fiber'
+import htm from 'https://esm.sh/htm'
+
+const html = htm.bind(React.createElement)
+
+function Thing() {
+    const ref = useRef()
+    useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01))
+    return html` <mesh
+            ref=${ref}
+            >
+            <boxGeometry args=${[1, 1, 1]} />
+            <meshNormalMaterial attach=${"material"} />
+            </mesh>`
+}
+
+export function RenderReact() {
+    ReactDOM.render(
+        html` <${Canvas}>
+                  <${Thing} />
+              </${Canvas}>`,
+        document.getElementById('blazorReact'),
+    )
+}
