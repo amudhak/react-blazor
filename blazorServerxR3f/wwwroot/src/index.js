62a7a240f6c5dacddd14b903344e94fae803ac7d
Amudha <amudha@Amudhas-MacBook-Pro.local>
2023-08-16 11:39:38 -0400

    webassembly added

--- blazorServerxR3f/wwwroot/src/index.js
+++ blazorServerxR3f/wwwroot/src/index.js
@@ -1,18 +1,26 @@
-﻿import React, { useRef } from 'react'
-import ReactDOM from 'react-dom'
-import { Canvas, useFrame } from 'react-three-fiber'
+﻿import React, { useRef } from 'https://esm.sh/react'
+import ReactDOM from 'https://esm.sh/react-dom'
+import { Canvas, useFrame } from 'https://esm.sh/@react-three/fiber'
+import htm from 'https://esm.sh/htm'
 
-export function Thing() {
+const html = htm.bind(React.createElement)
+
+function Thing() {
     const ref = useRef()
     useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01))
-    return (
-        <mesh
-            ref={ref}
-            onClick={e => console.log('click')}
-            onPointerOver={e => console.log('hover')}
-            onPointerOut={e => console.log('unhover')}>
-            <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-            <meshNormalMaterial attach="material" />
-        </mesh>
+    return html` <mesh
+            ref=${ref}
+            >
+            <boxGeometry args=${[1, 1, 1]} />
+            <meshNormalMaterial attach=${"material"} />
+            </mesh>`
+}
+
+export function RenderReact() {
+    ReactDOM.render(
+        html` <${Canvas}>
+                  <${Thing} />
+              </${Canvas}>`,
+        document.getElementById('blazorReact'),
     )
 }
